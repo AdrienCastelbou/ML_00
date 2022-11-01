6a117952ff1062d273bc20eba1396b35c811af97
Adrien CASTELBOU <acastelb@e3r4p16.clusters.42paris.fr>
2022-11-01 14:35:52 +0100

    ex02 ok

--- ex02/prediction.py
+++ ex02/prediction.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 def simple_predict(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
-
-    if type(x) != np.ndarray or type(theta) != np.ndarray or len(x) == 0 or len(theta) != 2:
+    try:
+        if type(x) != np.ndarray or type(theta) != np.ndarray or len(x) == 0 or len(theta) != 2:
+            return None
+        y_hat = np.zeros(len(x))
+        for i in range(0, len(x)):
+            y_hat[i] = theta[0] + x[i] * theta[1]
+        return y_hat
+    except:
         return None
-    y_hat = np.zeros(len(x))
-    for i in range(0, len(x)):
-        y_hat[i] = theta[0] + x[i] * theta[1]
-    return y_hat
