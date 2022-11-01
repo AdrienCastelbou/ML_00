feeced9187781f3e61188c3b8c475977f78008bb
Adrien CASTELBOU <acastelb@e3r4p16.clusters.42paris.fr>
2022-11-01 12:35:14 +0100

    all should be ok except percentile

--- ex01/TinyStatistician.py
+++ ex01/TinyStatistician.py
@@ -1,10 +1,9 @@
 from math import sqrt
-from numpy import array
+import numpy as np
+from typing import Union
 
 class TinyStatistician:
-    def mean(self, x):
-        if type(x) != list and type(x) != array:
-            return None
+    def mean(self, x: Union[list, np.array]) -> float:
         l = float(len(x))
         sum = 0.0
         for elem in x:
@@ -13,30 +12,29 @@ class TinyStatistician:
             sum += elem
         return sum / l
     
-    def median(self, x):
-        if type(x) != list and type(x) != array:
-            return None
+    def median(self, x: Union[list, np.array])  -> float:
         l = len(x)
         if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
             return None
         x.sort()
         return x[int((l + 1) / 2) - 1]
     
-    def quartile(self, x):
-        if type(x) != list and type(x) != array:
-            return None
+    def quartile(self, x: Union[list, np.array]):
         l = len(x)
         if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
             return None
         x.sort()
-        return [x[int((l + 3)/ 4) - 1], x[int((3 * l + 1) / 4)] - 1]
+        return [float(x[int((l + 3)/ 4) - 1]), float(x[int((3 * l + 1) / 4) - 1])]
     
-    def percentile(self, x):
+
+    def percentile(self, x: Union[list, np.array], p: Union[int, float]) -> float:
+        l = len(x)
+        if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
+            return None
+        x.sort()
         return None
     
-    def var(self, x):
-        if type(x) != list and type(x) != array:
-            return None
+    def var(self, x: Union[list, np.array]) -> float:
         l = len(x)
         if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
             return None
@@ -44,16 +42,20 @@ class TinyStatistician:
         sum = 0
         for elem in x:
             sum += (elem - mean) * (elem - mean)
-        return sum / l
+        return sum / (l  - 1)
     
-    def std(self, x):
-        if type(x) != list and type(x) != array:
-            return None
+    def std(self, x: Union[list, np.array]) -> float:
         l = len(x)
         if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
             return None
         return sqrt(self.var(x))
 
-t = TinyStatistician()
-
-print(t.std([2, 7, 3, 12, 9]))
+a = [1, 42, 300, 10, 59]
+#print(TinyStatistician().mean(a))
+#print(TinyStatistician().median(a))
+#print(TinyStatistician().quartile(a))
+print(TinyStatistician().percentile(a, 10))
+#print(TinyStatistician().percentile(a, 15))
+#print(TinyStatistician().percentile(a, 20))
+#print(TinyStatistician().var(a))
+#print(TinyStatistician().std(a))
