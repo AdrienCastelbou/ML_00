d75b3ff3ed9c3320714c12d0e95426acf4057c6d
Adrien CASTELBOU <acastelb@e3r4p16.clusters.42paris.fr>
2022-11-01 17:00:13 +0100

    6 ok

---
+++ ex06/loss.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def predict_(x, theta) -> np.array:
+    try:
+        if type(x) != np.ndarray or type(theta) != np.ndarray:
+            return None
+        if not len(x) or theta.shape != (2, 1):
+            return None
+        if x.ndim == 1:
+            x = x.reshape(x.shape[0], -1)
+        if x.shape[1] != 1:
+            return None
+        m = np.hstack((np.ones((x.shape[0], 1)), x))
+        return m.dot(theta)
+    except:
+        None
+
+
+def loss_elem_(y, y_hat):
+    J_elem = np.zeros(y.shape[0])
+    for i in range(0, y.shape[0]):
+        J_elem[i] = (y_hat[i] - y[i]) ** 2
+    return J_elem
+
+
+def loss_(y, y_hat):
+    J_elem = loss_elem_(y, y_hat)
+    sum = 0
+    for elem in J_elem:
+        sum += elem
+    J = (1 / (2 * y.shape[0])) * sum
+    return J
+
+
+def main_test():
+    x1 = np.array([[0.], [1.], [2.], [3.], [4.]])
+    theta1 = np.array([[2.], [4.]])
+    y_hat1 = predict_(x1, theta1)
+    y1 = np.array([[2.], [7.], [12.], [17.], [22.]])
+    # Example 1:
+    print(loss_elem_(y1, y_hat1))
+    print(loss_(y1, y_hat1))
+    x2 = np.array([0, 15, -9, 7, 12, 3, -21]).reshape(-1, 1)
+    theta2 = np.array([[0.], [1.]]).reshape(-1, 1)
+    y_hat2 = predict_(x2, theta2)
+    y2 = np.array([2, 14, -13, 5, 12, 4, -19]).reshape(-1, 1)
+    # Example 3:
+    loss_(y2, y_hat2)
+    print(loss_(y2, y_hat2))
+    print(loss_(y2, y2))
+
+if __name__ == "__main__":
+    main_test()
