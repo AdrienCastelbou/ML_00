c1e0e4561ff86dda8acd313801c9c1232a313502
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-02 14:22:26 +0100

    8 should be ok

--- ex08/plot.py
+++ ex08/plot.py
@@ -5,7 +5,6 @@ def loss_(y, y_hat):
     return float(1 / (2 * y.shape[0]) * (y_hat - y).T.dot(y_hat - y))
 
 def predict_(x, theta) -> np.array:
-    print(theta.shape)
     try:
         if type(x) != np.ndarray or type(theta) != np.ndarray:
             return None
@@ -22,21 +21,26 @@ def predict_(x, theta) -> np.array:
 
 
 def plot_with_loss(x, y, theta):
-    if type(y) != np.ndarray or len(y) == 0:
-        return
+    if type(y) != np.ndarray or type(y_hat) != np.ndarray or type(theta) != np.ndarray:
+        return None
     if y.ndim == 1:
-        y = y.reshape(x.shape[0], -1)
+        y = y.reshape(y.shape[0], -1)
+    if y_hat.ndim == 1:
+        y_hat = y_hat.reshape(y_hat.shape[0], -1)
     if theta.ndim == 1:
         theta = theta.reshape(theta.shape[0], -1)
-    if y.shape[1] != 1:
-        return
-    y_hat = predict_(x, theta)
-    plt.scatter(x, y)
-    plt.title(f"Cost: {2 * loss_(y, y_hat)}")
-    plt.plot(x, y_hat, color="orange")
-    for i in range(0, y.shape[0]):
-        plt.plot([x[i], x[i]], [y[i][0], y_hat[i][0]], 'r', linestyle="--")
-    plt.show()
+    if y.shape[1] != 1 or y_hat.shape[1] != 1 or theta.shape != (2, 1):
+        return None
+    try:
+        y_hat = predict_(x, theta)
+        plt.scatter(x, y)
+        plt.title(f"Cost: {2 * loss_(y, y_hat)}")
+        plt.plot(x, y_hat, color="orange")
+        for i in range(0, y.shape[0]):
+            plt.plot([x[i], x[i]], [y[i][0], y_hat[i][0]], 'r', linestyle="--")
+        plt.show()
+    except:
+        return 
 
 
 
