267bf402134e866fbe9782068b9846a19576baff
Adrien CASTELBOU <acastelb@e3r5p16.clusters.42paris.fr>
2022-11-02 17:17:39 +0100

    add test

--- ex04/prediction.py
+++ ex04/prediction.py
@@ -14,3 +14,24 @@ def predict_(x, theta) -> np.array:
         return m.dot(theta)
     except:
         None
+
+def main_test():
+    x = np.arange(1,6)
+    # Example 1:
+    theta1 = np.array([[5], [0]])
+    print(predict_(x, theta1))
+    # Do you remember why y_hat contains only 5’s here?
+    # Example 2:
+    theta2 = np.array([[0], [1]])
+    predict_(x, theta2)
+    # Do you remember why y_hat == x here?
+    # Example 3:
+    theta3 = np.array([[5], [3]])
+    print(predict_(x, theta3))
+    # Example 4:
+    theta4 = np.array([[-3], [1]])
+    print(predict_(x, theta4))
+
+
+if __name__ == "__main__":
+    main_test()
