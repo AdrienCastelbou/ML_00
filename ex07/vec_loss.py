b8921b2e0d4490d8da488cb8ceace59c25ea1155
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-02 14:24:48 +0100

    7 shoudl be clean

--- ex07/vec_loss.py
+++ ex07/vec_loss.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 def loss_(y, y_hat):
-    return float(1 / (2 * y.shape[0]) * (y_hat - y).T.dot(y_hat - y))
+    if type(y) != np.ndarray or type(y_hat) != np.ndarray:
+        return None
+    if y.ndim == 1:
+        y = y.reshape(y.shape[0], -1)
+    if y_hat.ndim == 1:
+        y_hat = y_hat.reshape(y_hat.shape[0], -1)
+    if y.shape[1] != 1 or y_hat.shape[1] != 1:
+        return None
+    try:
+        return float(1 / (2 * y.shape[0]) * (y_hat - y).T.dot(y_hat - y))
+    except:
+        return None
 
 def main_test():
     X = np.array([[0], [15], [-9], [7], [12], [3], [-21]])
