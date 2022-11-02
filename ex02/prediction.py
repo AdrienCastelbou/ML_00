267808f29c5ff72d3d2297918b144a12e2b86dac
Adrien CASTELBOU <acastelb@e3r5p16.clusters.42paris.fr>
2022-11-02 17:08:29 +0100

    add test

--- ex02/prediction.py
+++ ex02/prediction.py
@@ -10,3 +10,23 @@ def simple_predict(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
         return y_hat
     except:
         return None
+
+
+def main_test():
+    x = np.arange(1,6)
+    # Example 1:
+    theta1 = np.array([5, 0])
+    print(simple_predict(x, theta1))
+    # Example 2:
+    theta2 = np.array([0, 1])
+    print(simple_predict(x, theta2))
+    # Do you understand why y_hat == x here?
+    # Example 3:
+    theta3 = np.array([5, 3])
+    print(simple_predict(x, theta3))
+    # Example 4:
+    theta4 = np.array([-3, 1])
+    print(simple_predict(x, theta4))
+
+if __name__ == "__main__":
+    main_test()
