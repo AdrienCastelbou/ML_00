a4fb8b0c58499c71c6373b2a34a724a981838aec
Adrien Castelbou <acastelbou1@gmail.com>
2022-11-03 20:48:23 +0100

    fix median

--- ex01/TinyStatistician.py
+++ ex01/TinyStatistician.py
@@ -18,7 +18,14 @@ class TinyStatistician:
 
     def median(self, x: Union[list, np.array]) -> float:
         try:
-            return self.percentile(x, 50)
+            sorted_x = sorted(x)
+            l = len(x)
+            if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
+                return None
+            if l % 2:
+                return sorted_x[int(len(x) / 2)]
+            else:
+                return (sorted_x[int(len(x) / 2) - 1] + sorted_x[int(len(x) / 2)]) / 2
         except:
             return None
 
@@ -30,7 +37,7 @@ class TinyStatistician:
                 return None
             x = sorted(x)
             print(x)
-            return [x[int(l / 4)], x[int(l * 3 / 4)]]
+            return [float(x[int(l / 4)]), float(x[int(l * 3 / 4)])]
         except:
             return None
 
@@ -38,15 +45,9 @@ class TinyStatistician:
     def percentile(self, x: Union[list, np.array], p: Union[int, float]) -> float:
         try:
             l = len(x)
-            if l == 0 or not all(isinstance(elem, (int, float)) for elem in x):
-                return None
-            x = sorted(x)
-            r = (p/100) * (l - 1) + 1
-            ri = int(r)
-            rf = r - int(r)
-            if ri >= l:
-                return x[l - 1]
-            return x[ri - 1] + rf * (x[ri] - x[ri - 1])
+            sorted_x = sorted(x)
+            idx = int(np.ceil(p / 100 * l)) - 1
+            return sorted_x[idx]
         except:
             return None
 
